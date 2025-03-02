5cf4d2f258c4633e58670408d74ad861d2db4199
rajbupal <rajbupal.n@gmail.com>
2025-03-02 22:17:52 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+st.write("Hello")
+x=st.text_input("Fav movie")
+st.write(f"Fav movie is :- {x}")
+is_clicked = st.button("Click Me")
+st.link_button("profile" , "/profile?id=1234")
+#is_clicked.
+#st.write("Clicked")
+#data = pd.read_csv("C:\\Users\\e008902\\NPO\\NNI Mismatch_9 Jan 251.csv")
+#st.write(data)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a","b","c"]
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
